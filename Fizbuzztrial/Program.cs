d9c05ac76bad1a3b605b56719e3a63d1ac677f05
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-20 20:18:22 -0500

    Basic fizzBuzz solution

--- Fizbuzztrial/Program.cs
+++ Fizbuzztrial/Program.cs
@@ -9,8 +9,29 @@ namespace Fizbuzztrial
             int length = 100;
             for (int i = 1; i <= length; i++)
             {
-                Console.WriteLine(i);
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
             }
+
+            Console.ReadLine();
         }
     }
 }
