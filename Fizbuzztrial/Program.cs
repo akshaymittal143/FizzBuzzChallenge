8d120bb69f3696319b298ad26508e8ad58debb26
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-20 20:59:43 -0500

    Added library

--- Fizbuzztrial/Program.cs
+++ Fizbuzztrial/Program.cs
@@ -1,15 +1,13 @@
-﻿using System;
-
+﻿using FizzBuzz;
+using System;
 namespace Fizbuzztrial
 {
     class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 100; i++)
-            {
-                Console.WriteLine("{0}", i % 15 == 0 ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : i.ToString())));
-            }
+            FizzBuzzClass result = new FizzBuzzClass();
+            result.FizzBuzz();
             Console.ReadLine();
         }
     }
