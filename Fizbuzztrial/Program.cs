f7aaf546c8a2ee360093360b20689f877fec6a30
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-20 21:10:58 -0500

    support an arbitrary numeric range

--- Fizbuzztrial/Program.cs
+++ Fizbuzztrial/Program.cs
@@ -1,13 +1,15 @@
 ﻿using FizzBuzz;
 using System;
-namespace Fizbuzztrial
+namespace FizzBuzzProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a Maximum Number:");
+            int number = Convert.ToInt32(Console.ReadLine());
             FizzBuzzClass result = new FizzBuzzClass();
-            result.FizzBuzz();
+            result.FizzBuzz(number);
             Console.ReadLine();
         }
     }
