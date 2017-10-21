0b0e2cbf1f3e7dd3a19df0d30f98d997c31b2570
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-20 19:57:15 -0500

    print 1 to 100

--- Fizbuzztrial/Program.cs
+++ Fizbuzztrial/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fizbuzztrial
 {
@@ -10,6 +6,11 @@ namespace Fizbuzztrial
     {
         static void Main(string[] args)
         {
+            int length = 100;
+            for (int i = 1; i <= length; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
