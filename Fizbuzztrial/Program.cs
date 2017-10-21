386967f1ca5e8d82120f5a582ff7b584f69c2a72
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-20 20:41:22 -0500

    Better solution

--- Fizbuzztrial/Program.cs
+++ Fizbuzztrial/Program.cs
@@ -6,31 +6,10 @@ namespace Fizbuzztrial
     {
         static void Main(string[] args)
         {
-            int length = 100;
-            for (int i = 1; i <= length; i++)
+            for (int i = 1; i <= 100; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-
-                }
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("Fizz");
-
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-
-                }
-
-                else
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine("{0}", i % 15 == 0 ? "FizzBuzz" : (i % 3 == 0 ? "Fizz" : (i % 5 == 0 ? "Buzz" : i.ToString())));
             }
-
             Console.ReadLine();
         }
     }
